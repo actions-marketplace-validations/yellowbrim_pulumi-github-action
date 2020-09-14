d3c84d77ec777a5a5369f8107c55823a66d662b5
H. Charley Bodkin <hcbodkin@gmail.com>
2020-09-14 14:24:12 -0400

    feat: add an initial action api

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const yaml = require('js-yaml');
+const fs = require('fs');
+const { set } = require('lodash');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  core.setOutput('time', time);
+  const yqPath = core.getInput('yq-path');
+  const value = core.getInput('value');
+  const encoding = core.getInput('yml-file-encoding');
+  const ymlFileName = core.getInput('yml-file');
+
+  const doc = yaml.safeLoad(fs.readFileSync(ymlFileName, encoding), {
+    json: true,
+    filename: ymlFileName, // Just for debugging purposes, no funcational purpose ("string to be used as a file path in error/warning messages")
+  });
+  console.log(`Doc got!`, doc);
+  core.setOutput('originalDoc', JSON.stringify(doc));
+
+  const updatedDoc = set(doc, yqPath, value);
+  core.setOutput('updatedDoc', JSON.stringify(doc));
+  console.log(`Doc updated!`, updatedDoc);
+
+  fs.writeFileSync(yaml.safeDump(updatedDoc, {}));
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
